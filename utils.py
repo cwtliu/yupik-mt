598a7e4fa86078e068badc4dc0f00da03ace2f14
Temigo <temigo@gmx.com>
2018-01-28 17:42:41 -0800

    Update dictionary files with definitions.

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 import os
 
 def writeListToFile(list, filename):
-    f = open(filename, 'a')
+    f = open(filename, 'w')
     for x in list:
         f.write(x.encode('utf-8') + os.linesep)
     f.close()
