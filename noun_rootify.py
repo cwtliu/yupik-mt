679c5f308afe8f63be1017e4e0220400b65bf89a
cwtliu <christopher.w.liu@gmail.com>
2018-02-04 13:16:36 -0800

    concatenated verbs/nouns

--- noun_rootify.py
+++ noun_rootify.py
@@ -1,32 +1,32 @@
 
 vowels = ['a','e','i','u']
 consonants = ['p','t','c','k','q','v','l','s','y','g','r','m','n','g']
-with open('allnouns.txt','r') as fdata:
-    with open('allnouns_root.txt','w') as fout:
+with open('data/all_nouns.txt','r') as fdata:
+    with open('data/all_nouns_root.txt','w') as fout:
     	for line in fdata:
     		flag = False
     		line = line.strip()
     		line = line.replace(' ','')
-    		fout.write(line)
+    		#fout.write(line)
     		if line[-1:] == '*':
     			line = line[:-1]
     			flag = True
-    		if line[-1:] == 'k':
+    		elif line[-1:] == 'k':
     			fout.write(line[:-1])
     			fout.write('g-')
-    		if line[-1:] == 'q':
+    		elif line[-1:] == 'q':
     			fout.write(line[:-1])
     			fout.write('r-')
-    		if line[-1:] == 'i':
+    		elif line[-1:] == 'i':
     			fout.write(line)
     			fout.write('-')
-    		if line[-1:] == 'u':
+    		elif line[-1:] == 'u':
     			fout.write(line)
     			fout.write('-')
-    		if line[-1:] == 'a':
+    		elif line[-1:] == 'a':
     			fout.write(line)
     			fout.write('-')
-    		if len(line) > 1:
+    		elif len(line) > 1:
       			if line[-1:] == 'n' and line[-2] in vowels:
     				fout.write(line[:-2])
     				fout.write(line[-2])
@@ -34,12 +34,12 @@ with open('allnouns.txt','r') as fdata:
     			if line[-2:] == 'ae':
     				fout.write(line[:-2])
     				fout.write('e-')
-    		if len(line) > 2:
+    		elif len(line) > 2:
     			if line[-2:] == 'ta' and line[-3] in consonants:
     				fout.write(line[:-3])
     				fout.write(line[-3])
     				fout.write('te-')
-    		if len(line) > 5:
+    		elif len(line) > 5:
     			if line[-5:] == '(aq*)':
     				fout.write(line[:-5])
     				fout.write('ar-')
