e3e18b04ef98d56030c200627fcbf2d1a2397eab
Temigo <temigo@gmx.com>
2018-02-05 22:09:10 -0800

    Parse postbases dictionary and include definitions of postbases.

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+# *-* encoding: utf-8 *-*
 # Tools and utils
 import os
 
@@ -6,3 +7,13 @@ def writeListToFile(list, filename):
     for x in list:
         f.write(x.encode('utf-8') + os.linesep)
     f.close()
+
+def replaceSpecialCharacters(w):
+    w = w.replace(u'\u00F7', u'\u0144') # division to n acute
+    w = w.replace(u'\u00B5', u'\u1E3F') # micro to m acute
+    w = w.replace('TMg', u'\u0144g')# ng acute
+    # Ligatures : leave them with ¥ symbol
+    #w = w.replace(u'\u00A5g', u'\u011D') # ¥g to ug ligature (g circonflex)
+    #w = w.replace(u'\u00A5q', u'\u')# ur ligature
+    # urr ligature
+    return w
