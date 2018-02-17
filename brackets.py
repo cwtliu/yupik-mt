298d02b67334ab7a83cc2562da2175d981bc3701
Temigo <temigo@gmx.com>
2018-02-17 14:05:58 -0800

    Fix brackets removal. Remove space also.

--- brackets.py
+++ brackets.py
@@ -14,14 +14,11 @@ with open(filename + "_no_brackets", "w") as f:
         m = r.split(line)
         if len(m)>=3:
             new_line = ""
-            if len(m) > 3:
-                print line
-                print m
             for i in range(len(m)):
                 if i%2==0:
                     new_line += m[i]
                 elif len(m)%2 == 0 and i == len(m)-1:
                     new_line += m[i]
-            f.write(new_line.replace('[', '').replace(']', ''))
+            f.write(new_line.replace('[', '').replace(']', '').rstrip().lstrip() + '\n')
         else:
-            f.write(line.replace('[', '').replace(']', ''))
+            f.write(line.replace('[', '').replace(']', '').rstrip().lstrip() + '\n')
