2e63f7b2f893eb3b3b5408a903d9385e10739142
Temigo <temigo@gmx.com>
2018-02-25 22:20:54 -0800

    Parser works with postbases.

--- parser/parser.py
+++ parser/parser.py
@@ -62,49 +62,81 @@ class DirtyParser(object):
             nouns.append(f_nouns.readlines())
             verbs.append(f_verbs.readlines())
 
-        words = [w.rstrip('\n') for w in verbs[0]]
+        words = [w.rstrip('\n').rstrip('-') for w in verbs[0]]
         words.extend([w.rstrip('\n') for w in nouns[0]])
         self.dictionary = words
+
+    def compare(self, w1, w2):
+        """
+        w1 is the reference word
+        """
+        #if len(w2) < len(w1):
+        #    temp = w1
+        #    w1 = w2
+        #    w2 = temp
+        d = 0
+        for i in range(min(len(w1), len(w2))):
+            if w1[i] != w2[i]:
+                break
+            else:
+                d += 1
+        return d
 
     def match(self, word):
         # Check against dictionary
         matches = {}
         for w in self.dictionary:
-            d = 0
-            for i in range(len(w)):
-                if w[i] != word[i]:
-                    break
-                else:
-                    d += 1
+            d = self.compare(w, word)
             matches[w[:d+2]] = d
         best_match = max(matches.values())
         return [m for m in matches if matches[m] == best_match]
 
-    def parse(self, word):
+    def parse(self, word, match):
+        good = []
+        if self.debug: print "\n+ Root ", match
+        # Get the remaining of root and word
+        #remaining_word = ""
+        #remaining_root = ""
+        #for i in range(len(m)):
+        #    if word[i] != m[i]:
+        #        remaining_word = word[i:]
+        #        remaining_root = m[i:]
+        #        break
+
+        for postbase in self.postbases:
+            if self.debug: print "Postbase ", postbase
+            # Assuming one level only
+            new_word = postbase.concat(match)
+            #print(self.compare(new_word, word)-len(new_word))
+            good_match = abs(self.compare(new_word, word)-len(new_word)) <= 2
+            if self.debug: print good_match
+            #leftover_root, good_match = postbase.parse(m, remaining_word, remaining_root)
+            if good_match:
+                good.append((postbase, new_word))
+        return good
+
+    def analyze(self, word):
         if self.debug: print "Looking at ", word
         matches = self.match(word)
+        matches = [([m], m) for m in matches]
+        # Matches is a list of tuples (tokens, match)
+        # such that concatenation of tokens = match
+        final_matches = []
+        while len(matches):
+            tokens, match = matches.pop()
+            if self.debug: print("++++ Match %s ++++" % match, tokens)
+            if abs(self.compare(match, word)-len(match)) <= 2 and abs(len(word) - self.compare(match, word)) <= 2:
+                final_matches.append((tokens, match))
+            else:
+                good = self.parse(word, match)
+                matches = matches + [(tokens + [g[0]], g[1]) for g in good]
 
-        good = []
-        for m in matches:
-            if self.debug: print "\n+ Root ", m
-            # Get the remaining of root and word
-            remaining_word = ""
-            remaining_root = ""
-            for i in range(len(m)):
-                if w[i] != m[i]:
-                    remaining_word = w[i:]
-                    remaining_root = m[i:]
-                    break
-
-            for postbase in self.postbases:
-                if self.debug: print "Postbase ", postbase
-                leftover_root, good_match = postbase.parse(m, remaining_word, remaining_root)
-                if good_match:
-                    good.append((m, postbase, leftover_root))
-        return good
+        return final_matches
 
 if __name__ == '__main__':
     test_words = ["pissullrunrituq", "nerenrituq"]
     p = DirtyParser(debug=False)
-    for w in test_words:
-        print p.parse(w)
+    #for w in test_words:
+    #    print p.parse(w)
+    print(p.analyze("pissuryug"))
+    print(p.analyze("pissuryunriteqatartuq"))
